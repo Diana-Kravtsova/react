0fc32de6e8268ed5ea8cb92420a72b953ecf97c0
Diana-Kravtsova <dayana.kravtsova@gmail.com>
2023-03-27 22:02:32 +0300

    Add custom usePrompt hook

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,16 @@ import { useSelector } from 'react-redux';
 import styles from './Header.module.css';
 import Menu from './Menu';
 import ButtonMenu from './ButtonMenu';
+import usePrompt from '../../hooks/usePrompt';
 
 function Header() {
     const { type, email } = useSelector(state => state.auth);
+    const name = usePrompt(3000, 'What is your name?').trim();
 
     return (
         <div className={styles.header}>
             <Menu />
-            {type && <span>Hello, {email}!</span>}
+            {type && <span>Hello, {name || email}!</span>}
             <ButtonMenu />
         </div>
     );
